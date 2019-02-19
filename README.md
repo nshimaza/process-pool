77a15d123a5a4c0c8413051b9179aca64e39e031
Naoto Shimazaki <nshimaza@cisco.com>
2019-02-19 15:16:59 +0900

    add comments

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # process-pool
 Sample implementation of process pool using async-supervisor package
+
+This package implements a process pool described in [this page](https://learnyousomeerlang.com/building-applications-with-otp#a-pool-of-processes)
+using async-supervisor package.  This demonstrates how to use the package.
--- src/Control/Concurrent/ProcessPool.hs
+++ src/Control/Concurrent/ProcessPool.hs
@@ -7,18 +7,26 @@ import           UnliftIO
 
 import           Control.Concurrent.Supervisor
 
+{-|
+    `PoolMessage` defines all messages for pool manager actor.
+    It follows Server behavior rule from async-supervisor package.
+-}
 data PoolMessage
-    = ProcDown ExitReason
-    | Run (IO ()) (ServerCallback (Maybe (Async ())))
-    | RunSync (IO ()) (ServerCallback (Maybe (Async ())))
+    = ProcDown ExitReason                                   -- ^ Notification on process termination.
+    | Run (IO ()) (ServerCallback (Maybe (Async ())))       -- ^ Request to run a process.  Returns `Async` on success, `Nothing` on supervisor timeout or pool is full.
+    | RunSync (IO ()) (ServerCallback (Maybe (Async ())))   -- ^ Request to run a process.  Waits for available resource.  Returns `Async` on success, `Nothing` on timeout.
 
+-- | Type sysnonym of pool manager actor's write-end queue.
 type PoolQueue = Actor PoolMessage
 
-newProcessPool :: Int -> IO (Actor PoolMessage, IO ())
+-- | Create an actor of process pool
+newProcessPool
+    :: Int                              -- ^ Maximum number of processes runnable at a time.
+    -> IO (Actor PoolMessage, IO ())
 newProcessPool maxProcs = do
-    (workerSvQ, workerSv) <- newActor newSimpleOneForOneSupervisor
-    (poolManQ, poolMan) <- newActor $ poolManager workerSvQ maxProcs
-    (_, topSv) <- newActor $ newSupervisor OneForAll def
+    (workerSvQ, workerSv) <- newActor newSimpleOneForOneSupervisor      -- Supervisor for worker processes.
+    (poolManQ, poolMan) <- newActor $ poolManager workerSvQ maxProcs    -- Actor managing pool.
+    (_, topSv) <- newActor $ newSupervisor OneForAll def                -- Top level actor.
         [ newProcessSpec Permanent $ noWatch workerSv
         , newProcessSpec Permanent $ noWatch poolMan]
     pure (poolManQ, topSv)
@@ -52,11 +60,21 @@ poolManager workerSvQ maxProcs inbox = do
         let spec = newProcessSpec Temporary $ watch (\reason _ -> send (Actor inbox) $ ProcDown reason) action
         newChild def workerSvQ spec
 
+{-|
+    Request to run an IO action with pooled process.  Returns 'Async' on
+    success.  Returns 'Nothing' if pool is full or process creation timed out.
+-}
 run :: PoolQueue -> IO () -> IO (Maybe (Async ()))
 run pool action = join <$> call def pool (Run action)
 
+{-|
+    Request to run an IO action with pooled process.  Returns 'Async' on
+    success.  Wait until resource becomes available.  Returns 'Nothing' on
+    timoeout.
+-}
 runSync :: PoolQueue -> IO () -> IO (Maybe (Async ()))
 runSync pool action = join <$> call def pool (RunSync action)
 
+-- | Call runSync with wrapped by 'Async'.  Result (running process) is passed to continuation (callback).
 runAsync :: PoolQueue -> IO () -> (Maybe (Async ()) -> IO a) -> IO (Async a)
 runAsync pool action cont = async $ runSync pool action >>= cont
